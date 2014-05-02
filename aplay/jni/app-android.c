a3222d7fce07a2794e9b5ee3b1037fdac86d538d
Serge Voilokov <voilokov@gmail.com>
2014-05-02 16:15:45 -0400

    test apps added

---
+++ aplay/jni/app-android.c
@@ -0,0 +1,295 @@
+//#include <android/sensor.h>
+//#include <android/log.h>
+#include <android_native_app_glue.h>
+#include <stdlib.h>
+#include <errno.h>
+
+#include "../common/app.h"
+#include "../common/log.h"
+#include "../common/audio.h"
+
+int g_log_level = 0;
+const char* g_appname = "aplay";
+
+struct saved_state
+{
+    float angle;
+    int32_t x;
+    int32_t y;
+};
+
+struct engine
+{
+    struct android_app* app;
+    int32_t width;
+    int32_t height;
+    struct saved_state state;
+};
+
+static struct aengine aeng;
+static struct player bplayer;
+static struct player fplayer;
+static struct recorder arecorder;
+
+static void create_aengine()
+{
+    SLresult rc;
+
+    if (aeng.object)
+    {
+        LOGE("aengine already created");
+        return;
+    }
+    
+    rc = create_engine(&aeng);
+    
+    if (rc == -1)
+    {
+        LOGE("create_engine: rc: %d, errno: %d, slerror: %d", rc, errno, get_sl_error());
+    }
+    else
+    {
+        LOGI("aengine created");
+    }
+}
+
+void play_file()
+{
+    int rc;
+
+    if (!fplayer.object)
+    {
+        rc = create_file_player(&fplayer, "/sdcard/aplay.mp3", &aeng);
+
+        if (rc == -1)
+        {
+            LOGE("create_file_player: rc: %d, errno: %d, slerror: %d", rc, errno, get_sl_error());
+        }
+        else
+        {
+            LOGI("file player created");
+        }
+    }
+
+    rc = set_play_state(&fplayer, 1);
+    if (rc == -1)
+    {
+        LOGE("set_play_state: rc: %d, errno: %d, slerror: %d", rc, errno, get_sl_error());
+    }
+    else
+    {
+        LOGI("set_play_state(1)");
+    }
+}
+
+static void record_buffer()
+{
+    int rc;
+
+    if (!arecorder.object)
+    {
+        rc = create_recorder(&arecorder, &aeng);
+
+        if (rc == -1)
+        {
+            LOGE("create_recorder: rc: %d, errno: %d, slerror: %d", rc, errno, get_sl_error());
+        }
+        else
+        {
+            LOGI("recorder created");
+        }
+    }
+    
+    rc = start_recording(&arecorder);
+
+    if (rc == -1)
+    {
+        LOGE("start_recording: rc: %d, errno: %d, slerror: %d", rc, errno, get_sl_error());
+    }
+    else
+    {
+        LOGI("start_recording ok");
+    }
+}
+
+static void play_buffer()
+{
+    int rc;
+
+    if (!bplayer.object)
+    {
+        rc = create_queued_player(&bplayer, &aeng);
+
+        if (rc == -1)
+        {
+            LOGE("create_queued_player: rc: %d, errno: %d, slerror: %d", rc, errno, get_sl_error());
+        }
+        else
+        {
+            LOGI("qplayer created");
+        }
+    }
+
+    rc =  play_recording(&bplayer);
+
+    if (rc == -1)
+    {
+        LOGE("play_recording: rc: %d, errno: %d, slerror: %d", rc, errno, get_sl_error());
+    }
+    else
+    {
+        LOGI("play_recording ok");
+    }
+    
+    rc = set_play_state(&bplayer, 1);
+    
+    if (rc == -1)
+    {
+        LOGE("set_play_state: rc: %d, errno: %d, slerror: %d", rc, errno, get_sl_error());
+    }
+    else
+    {
+        LOGI("set_play_state(1)");
+    }
+}
+
+static void destroy_all()
+{
+    destroy_recorder(&arecorder);
+    destroy_player(&fplayer);
+    destroy_player(&bplayer);
+    destroy_engine(&aeng);
+}
+
+void process_key_event(AInputEvent* e)
+{
+    int rc;
+    int32_t key_code, action;
+    int64_t event_time;
+
+    key_code = AKeyEvent_getKeyCode(e);
+    event_time = AKeyEvent_getEventTime(e);
+    action = AKeyEvent_getAction(e);
+    
+    LOGT("key: time: %ld, key_code: %d, action: %d", event_time, key_code, action);
+    
+    if (action != AKEY_EVENT_ACTION_DOWN)
+        return;
+    
+    switch (key_code)
+    {
+    case AKEYCODE_E:
+        create_aengine();
+        break;
+
+    case AKEYCODE_D:
+        destroy_all();
+        break;
+
+    case AKEYCODE_F:
+        play_file();
+        break;
+
+    case AKEYCODE_R:
+        record_buffer();
+        break;
+
+    case AKEYCODE_B:
+        play_buffer();
+        break;
+    }
+}
+
+static int32_t engine_handle_input(struct android_app* app, AInputEvent* e)
+{
+    struct engine* engine;
+    int32_t type, devid, src;
+    float x, y;
+
+    engine = (struct engine*)app->userData;
+    
+    type = AInputEvent_getType(e);
+    devid = AInputEvent_getDeviceId(e);
+    src = AInputEvent_getSource(e);
+    
+    if (type == AINPUT_EVENT_TYPE_KEY)
+    {
+        process_key_event(e);
+    }
+
+    return 0;
+}
+
+static void engine_handle_cmd(struct android_app* app, int32_t cmd)
+{
+    struct engine* engine = (struct engine*)app->userData;
+    LOGT("cmd: %d", cmd);
+
+    switch (cmd)
+    {
+        case APP_CMD_SAVE_STATE:
+            engine->app->savedState = malloc(sizeof(struct saved_state));
+            *((struct saved_state*)engine->app->savedState) = engine->state;
+            engine->app->savedStateSize = sizeof(struct saved_state);
+            break;
+
+        case APP_CMD_INIT_WINDOW:
+            if (engine->app->window != NULL)
+            {
+            }
+            break;
+
+        case APP_CMD_TERM_WINDOW:
+            break;
+
+        case APP_CMD_GAINED_FOCUS:
+            break;
+        
+        case APP_CMD_LOST_FOCUS:
+            break;
+    }
+}
+
+void android_main(struct android_app* state)
+{
+    struct engine engine;
+
+    app_dummy();
+    LOGT("android_main started");
+
+    memset(&aeng, 0, sizeof(struct aengine));
+    memset(&fplayer, 0, sizeof(struct player));
+    memset(&bplayer, 0, sizeof(struct player));
+    memset(&arecorder, 0, sizeof(struct recorder));
+    
+    memset(&engine, 0, sizeof(engine));
+    state->userData = &engine;
+    state->onAppCmd = engine_handle_cmd;
+    state->onInputEvent = engine_handle_input;
+    engine.app = state;
+
+    if (state->savedState != NULL)
+    {
+        engine.state = *(struct saved_state*)state->savedState;
+    }
+
+    while (1)
+    {
+        int ident;
+        int events;
+        struct android_poll_source* source;
+
+        while ((ident = ALooper_pollAll(-1, NULL, &events, (void**)&source)) >= 0) 
+        {
+            if (source != NULL)
+            {
+                source->process(state, source);
+            }
+
+            if (state->destroyRequested != 0)
+            {
+                return;
+            }
+        }
+    }
+}
---
+++ aplay/jni/audio.c
@@ -0,0 +1,457 @@
+#include <jni.h>
+#include <assert.h>
+#include <string.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+
+#include <sys/types.h>
+//#include <android/asset_manager.h>
+//#include <android/asset_manager_jni.h>
+
+#include "../common/audio.h"
+
+static SLresult sl_error = 0;
+
+SLresult get_sl_error()
+{
+    SLresult ret = sl_error;
+    sl_error = 0;
+    return ret;
+}
+
+static const SLEnvironmentalReverbSettings reverbSettings = SL_I3DL2_ENVIRONMENT_PRESET_STONECORRIDOR;
+
+#define RECORDER_FRAMES (16000 * 5) // 5 seconds of recorded audio at 16 kHz mono, 16-bit signed little endian
+static short recorderBuffer[RECORDER_FRAMES];
+static unsigned recorderSize = 0;
+static SLmilliHertz recorderSR;
+
+// pointer and size of the next player buffer to enqueue, and number of remaining buffers
+static short *nextBuffer;
+static unsigned nextSize;
+static int nextCount;
+
+void bq_player_callback(SLAndroidSimpleBufferQueueItf bq, void *context)
+{
+    SLresult rc;
+    struct player* p = (struct player*)context;
+    bq;
+    context;
+    rc = (*p->play)->SetPlayState(p->play, SL_PLAYSTATE_STOPPED);
+}
+
+
+void bq_recorder_callback(SLAndroidSimpleBufferQueueItf bq, void *context)
+{
+    SLresult rc;
+    struct recorder* p = (struct recorder*)context;
+    bq;
+    context;
+
+    rc = (*p->record)->SetRecordState(p->record, SL_RECORDSTATE_STOPPED);
+}
+
+void destroy_player(struct player* p)
+{
+    if (p->object)
+        (*p->object)->Destroy(p->object);
+
+    memset(p, 0, sizeof(struct player));
+}
+
+void destroy_recorder(struct recorder* p)
+{
+    if (p->object)
+        (*p->object)->Destroy(p->object);
+
+    memset(p, 0, sizeof(struct recorder));
+}
+
+void destroy_engine(struct aengine* ae)
+{
+    if (ae->mix)
+        (*ae->mix)->Destroy(ae->mix);
+    
+    if (ae->object)
+        (*ae->object)->Destroy(ae->object);
+    
+    memset(ae, 0, sizeof(struct aengine));
+}
+
+int create_engine(struct aengine* ae)
+{
+    SLInterfaceID ids[1] = { SL_IID_ENVIRONMENTALREVERB };
+    SLboolean req[1] = { SL_BOOLEAN_FALSE };
+
+    SLresult rc;
+    
+    memset(ae, 0, sizeof(struct aengine));
+
+    rc = slCreateEngine(&ae->object, 0, NULL, 0, NULL, NULL);
+    if (SL_RESULT_SUCCESS != rc)
+        goto error;
+
+    rc = (*ae->object)->Realize(ae->object, SL_BOOLEAN_FALSE);
+    if (SL_RESULT_SUCCESS != rc)
+        goto error;
+
+    rc = (*ae->object)->GetInterface(ae->object, SL_IID_ENGINE, &ae->engine);
+    if (SL_RESULT_SUCCESS != rc)
+        goto error;
+
+    rc = (*ae->engine)->CreateOutputMix(ae->engine, &ae->mix, 1, ids, req);
+    if (SL_RESULT_SUCCESS != rc)
+        goto error;
+
+    rc = (*ae->mix)->Realize(ae->mix, SL_BOOLEAN_FALSE);
+    if (SL_RESULT_SUCCESS != rc)
+        goto error;
+
+    rc = (*ae->mix)->GetInterface(ae->mix, SL_IID_ENVIRONMENTALREVERB, &ae->reverb);
+    if (SL_RESULT_SUCCESS == rc)
+        (*ae->reverb)->SetEnvironmentalReverbProperties(ae->reverb, &reverbSettings);
+
+    return 0;
+    
+error:
+
+    destroy_engine(ae);
+    sl_error = rc;
+
+    return -1;
+}
+
+int create_queued_player(struct player* p, struct aengine* ae)
+{
+    SLresult rc;
+
+    SLDataLocator_AndroidSimpleBufferQueue loc_bufq = {
+        SL_DATALOCATOR_ANDROIDSIMPLEBUFFERQUEUE,
+        2
+    };
+    
+    SLDataFormat_PCM format_pcm = {
+        SL_DATAFORMAT_PCM,
+        1,
+        SL_SAMPLINGRATE_16,
+        SL_PCMSAMPLEFORMAT_FIXED_16,
+        SL_PCMSAMPLEFORMAT_FIXED_16,
+        SL_SPEAKER_FRONT_CENTER,
+        SL_BYTEORDER_LITTLEENDIAN
+    };
+
+    SLDataSource audioSrc = {
+        &loc_bufq,
+        &format_pcm
+    };
+
+    SLDataLocator_OutputMix loc_outmix = {
+        SL_DATALOCATOR_OUTPUTMIX,
+        ae->mix
+    };
+    
+    SLDataSink audioSnk = {
+        &loc_outmix,
+        NULL
+    };
+
+    SLInterfaceID ids[3] = {
+        SL_IID_BUFFERQUEUE,
+        SL_IID_EFFECTSEND,
+        SL_IID_VOLUME
+    };
+
+    SLboolean req[3] = {
+        SL_BOOLEAN_TRUE,
+        SL_BOOLEAN_TRUE,
+        SL_BOOLEAN_TRUE
+    };
+
+    memset(p, 0, sizeof(struct player));
+
+    rc = (*ae->engine)->CreateAudioPlayer(ae->engine, &p->object, &audioSrc, &audioSnk, 3, ids, req);
+    if (SL_RESULT_SUCCESS != rc)
+        goto error;
+
+    rc = (*p->object)->Realize(p->object, SL_BOOLEAN_FALSE);
+    if (SL_RESULT_SUCCESS != rc)
+        goto error;
+
+    rc = (*p->object)->GetInterface(p->object, SL_IID_PLAY, &p->play);
+    if (SL_RESULT_SUCCESS != rc)
+        goto error;
+
+    rc = (*p->object)->GetInterface(p->object, SL_IID_BUFFERQUEUE, &p->queue);
+    if (SL_RESULT_SUCCESS != rc)
+        goto error;
+
+    rc = (*p->queue)->RegisterCallback(p->queue, bq_player_callback, p);
+    if (SL_RESULT_SUCCESS != rc)
+        goto error;
+
+    rc = (*p->object)->GetInterface(p->object, SL_IID_EFFECTSEND, &p->effect_send);
+    if (SL_RESULT_SUCCESS != rc)
+        goto error;
+
+    rc = (*p->object)->GetInterface(p->object, SL_IID_VOLUME, &p->volume);
+    if (SL_RESULT_SUCCESS != rc)
+        goto error;
+
+    rc = (*p->play)->SetPlayState(p->play, SL_PLAYSTATE_PLAYING);
+    if (SL_RESULT_SUCCESS != rc)
+        goto error;
+        
+    return 0;
+    
+error:
+    
+    destroy_player(p);
+    sl_error = rc;
+    
+    return -1;
+}
+
+int create_file_player(struct player* p, const char* fname, struct aengine* ae)
+{
+    SLresult rc;
+    int fd;
+    struct stat st;
+    
+    memset(p, 0, sizeof(struct player));
+
+    fd = stat(fname, &st);
+    if (fd <= 0)
+        return -1;
+
+    fd = open(fname, O_RDONLY);
+    if (fd <= 0)
+        return -1;
+
+    SLDataLocator_AndroidFD loc_fd = {
+        SL_DATALOCATOR_ANDROIDFD,
+        fd, 
+        0, 
+        st.st_size
+    };
+    
+    SLDataFormat_MIME format_mime = {
+        SL_DATAFORMAT_MIME,
+        NULL,
+        SL_CONTAINERTYPE_UNSPECIFIED
+    };
+    
+    SLDataSource audioSrc = {
+        &loc_fd,
+        &format_mime
+    };
+
+    SLDataLocator_OutputMix loc_outmix = {
+        SL_DATALOCATOR_OUTPUTMIX, 
+        ae->mix
+    };
+    
+    SLDataSink audioSnk = {
+        &loc_outmix,
+        NULL
+    };
+
+    const SLInterfaceID ids[3] = {
+        SL_IID_SEEK, 
+        SL_IID_MUTESOLO, 
+        SL_IID_VOLUME
+    };
+    
+    const SLboolean req[3] = {
+        SL_BOOLEAN_TRUE, 
+        SL_BOOLEAN_TRUE, 
+        SL_BOOLEAN_TRUE
+    };
+    
+    rc = (*ae->engine)->CreateAudioPlayer(ae->engine, &p->object, &audioSrc, &audioSnk, 3, ids, req);
+    if (SL_RESULT_SUCCESS != rc)
+        goto error;
+
+    rc = (*p->object)->Realize(p->object, SL_BOOLEAN_FALSE);
+    if (SL_RESULT_SUCCESS != rc)
+        goto error;
+
+    rc = (*p->object)->GetInterface(p->object, SL_IID_PLAY, &p->play);
+    if (SL_RESULT_SUCCESS != rc)
+        goto error;
+
+    rc = (*p->object)->GetInterface(p->object, SL_IID_SEEK, &p->seek);
+    if (SL_RESULT_SUCCESS != rc)
+        goto error;
+
+    rc = (*p->object)->GetInterface(p->object, SL_IID_MUTESOLO, &p->solo);
+    if (SL_RESULT_SUCCESS != rc)
+        goto error;
+
+    rc = (*p->object)->GetInterface(p->object, SL_IID_VOLUME, &p->volume);
+    if (SL_RESULT_SUCCESS != rc)
+        goto error;
+
+    rc = (*p->seek)->SetLoop(p->seek, SL_BOOLEAN_TRUE, 0, SL_TIME_UNKNOWN);
+    if (SL_RESULT_SUCCESS != rc)
+        goto error;
+
+    return 0;
+
+error:
+
+    destroy_player(p);
+    sl_error = rc;
+
+    return -1;
+}
+
+int set_play_state(struct player* p, int isPlaying)
+{
+    SLresult rc;
+
+    if (!p->play)
+        return -1;
+
+    rc = (*p->play)->SetPlayState(p->play, isPlaying ? SL_PLAYSTATE_PLAYING : SL_PLAYSTATE_PAUSED);
+    if (SL_RESULT_SUCCESS == rc)
+        return 0;
+
+    sl_error = rc;
+    return -1;
+}
+
+int create_recorder(struct recorder* p, struct aengine* ae)
+{
+    SLresult rc;
+
+    SLDataLocator_IODevice loc_dev = {
+        SL_DATALOCATOR_IODEVICE, 
+        SL_IODEVICE_AUDIOINPUT,
+        SL_DEFAULTDEVICEID_AUDIOINPUT, 
+        NULL
+    };
+    
+    SLDataSource audioSrc = {
+        &loc_dev, 
+        NULL
+    };
+
+    SLDataLocator_AndroidSimpleBufferQueue loc_bq = {
+        SL_DATALOCATOR_ANDROIDSIMPLEBUFFERQUEUE,
+        2
+    };
+    
+    SLDataFormat_PCM format_pcm = {
+        SL_DATAFORMAT_PCM, 
+        1, 
+        SL_SAMPLINGRATE_16,
+        SL_PCMSAMPLEFORMAT_FIXED_16, 
+        SL_PCMSAMPLEFORMAT_FIXED_16,
+        SL_SPEAKER_FRONT_CENTER, 
+        SL_BYTEORDER_LITTLEENDIAN
+    };
+    
+    SLDataSink audioSnk = {
+        &loc_bq, 
+        &format_pcm
+    };
+
+    const SLInterfaceID id[1] = {
+        SL_IID_ANDROIDSIMPLEBUFFERQUEUE
+    };
+    
+    const SLboolean req[1] = {
+        SL_BOOLEAN_TRUE
+    };
+    
+    rc = (*ae->engine)->CreateAudioRecorder(ae->engine, &p->object, &audioSrc, &audioSnk, 1, id, req);
+    if (SL_RESULT_SUCCESS != rc)
+        goto error;
+
+    rc = (*p->object)->Realize(p->object, SL_BOOLEAN_FALSE);
+    if (SL_RESULT_SUCCESS != rc)
+        goto error;
+
+    rc = (*p->object)->GetInterface(p->object, SL_IID_RECORD, &p->record);
+    if (SL_RESULT_SUCCESS != rc)
+        goto error;
+
+    rc = (*p->object)->GetInterface(p->object, SL_IID_ANDROIDSIMPLEBUFFERQUEUE, &p->queue);
+    if (SL_RESULT_SUCCESS != rc)
+        goto error;
+
+    rc = (*p->queue)->RegisterCallback(p->queue, bq_recorder_callback, p);
+    if (SL_RESULT_SUCCESS != rc)
+        goto error;
+
+    return 0;
+
+error:
+
+    destroy_recorder(p);
+    sl_error = rc;
+
+    return -1;
+}
+
+int start_recording(struct recorder* p)
+{
+    SLresult rc;
+
+    rc = (*p->record)->SetRecordState(p->record, SL_RECORDSTATE_STOPPED);
+    if (SL_RESULT_SUCCESS != rc)
+        goto error;
+
+    rc = (*p->queue)->Clear(p->queue);
+    if (SL_RESULT_SUCCESS != rc)
+        goto error;
+
+    recorderSize = RECORDER_FRAMES * sizeof(short);
+
+    rc = (*p->queue)->Enqueue(p->queue, recorderBuffer, recorderSize);
+    if (SL_RESULT_SUCCESS != rc)
+        goto error;
+
+    rc = (*p->record)->SetRecordState(p->record, SL_RECORDSTATE_RECORDING);
+    if (SL_RESULT_SUCCESS != rc)
+        goto error;
+    
+    return 0;
+
+error:
+    
+    sl_error = rc;
+    
+    return -1;
+}
+
+int play_recording(struct player* p)
+{
+    SLresult rc;
+    
+    rc = (*p->queue)->Enqueue(p->queue, nextBuffer, nextSize);
+    
+    if (SL_RESULT_SUCCESS != rc)
+    {
+        sl_error = rc;
+        return -1;
+    }
+    
+    return 0;
+}
+
+int play_data(struct recorder* p, unsigned char* data, int size)
+{
+    SLresult rc;
+    
+    rc = (*p->queue)->Enqueue(p->queue, data, size);
+    
+    if (SL_RESULT_SUCCESS != rc)
+    {
+        sl_error = rc;
+        return -1;
+    }
+    
+    return 0;
+}
