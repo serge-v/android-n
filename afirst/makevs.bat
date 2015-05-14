4a42bc55b6b316ac217b5b3c3a111efccaa487d8
Serge Voilokov <svoilokov@appnexus.com>
2015-05-13 23:23:14 -0400

    removing old files

--- afirst/makevs.bat
+++
@@ -1,3 +0,0 @@
-set PATH=c:\cygwin\bin
-call ..\set_env.bat
-c:\cygwin\bin\make %1
--- common/Android.mk
+++
@@ -1,9 +0,0 @@
-LOCAL_PATH:= $(call my-dir)
-
-include $(CLEAR_VARS)
-
-LOCAL_MODULE    := common
-LOCAL_SRC_FILES := audio.c pic.c
-LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)
-
-include $(BUILD_STATIC_LIBRARY)
